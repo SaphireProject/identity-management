ad16cc3156562b19f94dfdb5d7be13349f8b45c8
Mikhail <starkin_mihail@mail.ru>
2019-02-18 09:36:25 +0300

    [TG-36] forgot one annotation

--- src/main/java/idm/controller/UserController.java
+++ src/main/java/idm/controller/UserController.java
@@ -7,12 +7,11 @@ import idm.model.UserDto;
 import idm.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
 
